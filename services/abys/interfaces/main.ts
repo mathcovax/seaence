cec1be189b82cf164972d44cd6ee6da7c9d37eef
ZeRiix <willflo46@gmail.com>
2025-03-16 18:30:04 +0100

    add: abys config

---
+++ services/abys/interfaces/main.ts
@@ -0,0 +1,20 @@
+import "@duplojs/node";
+import "@duplojs/node/globals";
+import { Duplo, useProcessBuilder, useRouteBuilder } from "@duplojs/core";
+import { envs } from "./envs";
+import "./routes";
+
+const duplo = new Duplo({
+	environment: envs.ENVIROMENT,
+	host: envs.HOST,
+	port: envs.PORT,
+});
+
+duplo.register(
+	...useProcessBuilder.getAllCreatedProcess(),
+	...useRouteBuilder.getAllCreatedRoute(),
+);
+
+await duplo.launch(
+	() => void console.log("Abys service is running !"),
+);
