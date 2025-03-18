3873fb7c99a837cd04ca0e7549483a10654464d3
ZeRiix <willflo46@gmail.com>
2025-03-18 17:39:04 +0100

    refacto(01): update commit config

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,22 +4,12 @@ export default {
 		"type-enum": [2, "always", ["feat", "fix", "doc", "refacto", "github", "config"]],
 		"scope-empty": [0],
 		"subject-case": [0],
-		"header-max-length": [2, "always", 100],
-		"header-pattern": [
-			2,
-			"always",
-			/^(feat|fix|doc|refacto|github|config)(\([0-9]+|hf\)|:(\w+)(\([0-9]+|hf\))?):.+$/
-		],
-		"scope-pattern": [
-			0,
-			"always",
-			/(^[0-9]+$|^hf$|^\w+\([0-9]+|hf\)$)/
-		],
+		"header-max-length": [2, "always", 100]
 	},
 	parserPreset: {
 		parserOpts: {
-			headerPattern: /^(feat|fix|doc|refacto|github|config)(\([0-9]+|hf\)|:(\w+)(\([0-9]+|hf\))?):\s(.*)$/,
-			headerCorrespondence: ["type", "scope", "subject"],
-		},
-	},
+			headerPattern: /^(feat|fix|doc|refacto|github|config)(?:\(([0-9]+|hf)\)|:(\w+)(?:\(([0-9]+|hf)\))?):\s(.*)$/,
+			headerCorrespondence: ["type", "scope", "breaking", "issue", "subject"]
+		}
+	}
 };
