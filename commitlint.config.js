0e5e1de5b8b70c56b124c7319dc3c5b0fac6f7b6
ZeRiix <willflo46@gmail.com>
2025-03-18 16:50:12 +0100

    refacto(01): exclude tunit vendors tsconfig

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 export default {
 	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [2, "always", ["feat", "fix", "doc", "refacto", "github", "config"]],
+		"scope-empty": [0],
+		"subject-case": [0],
+		"header-max-length": [2, "always", 100],
+		"header-pattern": [
+			2,
+			"always",
+			/^(feat|fix|doc|refacto|github|config)(\([0-9]+|hf\)|:(\w+)(\([0-9]+|hf\))?):.+$/
+		],
+		"scope-pattern": [
+			0,
+			"always",
+			/(^[0-9]+$|^hf$|^\w+\([0-9]+|hf\)$)/
+		],
+	},
+	parserPreset: {
+		parserOpts: {
+			headerPattern: /^(feat|fix|doc|refacto|github|config)(\([0-9]+|hf\)|:(\w+)(\([0-9]+|hf\))?):\s(.*)$/,
+			headerCorrespondence: ["type", "scope", "subject"],
+		},
+	},
 };
