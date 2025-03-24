58ef7b48945a44fb1015b81e38a72d3954ce8974
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-24 16:25:03 +0000

    feat(12): add default layout

---
+++ libs/design-system/src/components/DSButtonOutline.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DSButton from './ui/button/DSButton.vue';
+</script>
+
+<template>
+	<DSButton variant="outline" class="text-primary">
+		<slot />
+	</DSButton>
+</template>
--- services/spotter/src/components/TheFooter.vue
+++ services/spotter/src/components/TheFooter.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-	<footer>Footer</footer>
+	<footer class="bg-white">
+		<div class="container py-8">
+			<nav>
+				<ul class="flex gap-4 md:gap-8">
+					<li>
+						<RouterLink to="">Conditions d'utilisation</RouterLink>
+					</li>
+					<li>
+						<RouterLink to="">À propos</RouterLink>
+					</li>
+					<li>
+						<RouterLink to="">Support</RouterLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</footer>
 </template>
