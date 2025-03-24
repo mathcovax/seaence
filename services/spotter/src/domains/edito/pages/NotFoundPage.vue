51c0d8c754decfaa4590a9c2c2bd49985aa5e425
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-24 16:27:47 +0000

    feat(12): add home page + 404

--- services/spotter/src/domains/edito/pages/NotFoundPage.vue
+++ services/spotter/src/domains/edito/pages/NotFoundPage.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-
+const { HOME_PAGE } = routerPageName;
 </script>
 
 <template>
-	<div>
-		<h1>Not Found Page</h1>
-	</div>
+	<section class="flex flex-col items-center justify-center h-[calc(100vh-6rem-2rem)] gap-4">
+		<img src="/images/edito/404.png" alt="404" class="w-full max-w-2xs" />
+		<h1 class="text-4xl font-bold text-center">404 - Page non trouvée</h1>
+		<p class="text-center text-muted-foreground">La page que vous cherchez n'existe pas.</p>
+		<DSButtonPrimary as-child>
+			<RouterLink :to="{ name: HOME_PAGE }">Retour à l'accueil</RouterLink>
+		</DSButtonPrimary>
+	</section>
 </template>
