1a6879b3041c4587b1ecf5e400aa554afcf4262a
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-25 10:10:57 +0000

    feat: add auth pages

---
+++ libs/design-system/src/components/ui/label/index.ts
@@ -0,0 +1,1 @@
+export { default as DSLabel } from "./DSLabel.vue";
--- services/spotter/src/router/routerPageName/index.ts
+++ services/spotter/src/router/routerPageName/index.ts
@@ -1,5 +1,7 @@
 import { routerPageNameMain } from "@/domains/edito/router";
+import { routerPageNameAuth } from "@/domains/auth/router";
 
 export const routerPageName = Object.freeze({
 	...routerPageNameMain,
+	...routerPageNameAuth,
 });
