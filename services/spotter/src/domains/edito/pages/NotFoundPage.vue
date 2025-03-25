3ab8a151e95290db9070f4e9ef6d614131c8f40d
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-25 10:10:57 +0000

    feat: add auth pages

--- services/spotter/src/domains/edito/pages/NotFoundPage.vue
+++ services/spotter/src/domains/edito/pages/NotFoundPage.vue
@@ -3,12 +3,27 @@ const { HOME_PAGE } = routerPageName;
 </script>
 
 <template>
-	<section class="flex flex-col items-center justify-center h-[calc(100vh-6rem-2rem)] gap-4">
-		<img src="/images/edito/404.png" alt="404" class="w-full max-w-2xs" />
-		<h1 class="text-4xl font-bold text-center">404 - Page non trouvée</h1>
-		<p class="text-center text-muted-foreground">La page que vous cherchez n'existe pas.</p>
+	<section
+		class="flex flex-col items-center justify-center h-[calc(100vh-6rem-2rem)] gap-4"
+	>
+		<img
+			src="/images/edito/404.png"
+			alt="404"
+			class="w-full max-w-2xs"
+		>
+
+		<h1 class="text-4xl font-bold text-center">
+			{{ $t("page.notFound.title") }}
+		</h1>
+
+		<p class="text-center text-muted-foreground">
+			{{ $t("page.notFound.subtitle") }}
+		</p>
+
 		<DSButtonPrimary as-child>
-			<RouterLink :to="{ name: HOME_PAGE }">Retour à l'accueil</RouterLink>
+			<RouterLink :to="{ name: HOME_PAGE }">
+				{{ $t("cta.backHome") }}
+			</RouterLink>
 		</DSButtonPrimary>
 	</section>
 </template>
