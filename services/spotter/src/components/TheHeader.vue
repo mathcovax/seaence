a484fd0b3f2de0c2c59912db852f4d77a11fcf20
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-25 09:26:42 +0000

    fix(12): clean

--- services/spotter/src/components/TheHeader.vue
+++ services/spotter/src/components/TheHeader.vue
@@ -6,15 +6,23 @@ const { HOME_PAGE, REGISTER_PAGE, LOGIN_PAGE } = routerPageName;
 	<header class="sticky top-0 left-0 z-50 bg-white">
 		<div class="container h-24 flex items-center justify-between">
 			<RouterLink :to="{ name: HOME_PAGE }">
-				<img src="/images/logos/logo-text.svg" alt="Spotter" />
+				<img
+					src="/images/logos/logo-text.svg"
+					alt="Spotter"
+				>
 			</RouterLink>
 
 			<div class="hidden md:block space-x-2">
 				<DSButtonOutline as-child>
-					<RouterLink :to="{ name: REGISTER_PAGE }">S'inscrire</RouterLink>
+					<RouterLink :to="{ name: REGISTER_PAGE }">
+						S'inscrire
+					</RouterLink>
 				</DSButtonOutline>
+
 				<DSButtonPrimary as-child>
-					<RouterLink :to="{ name: LOGIN_PAGE }">Se connecter</RouterLink>
+					<RouterLink :to="{ name: LOGIN_PAGE }">
+						Se connecter
+					</RouterLink>
 				</DSButtonPrimary>
 			</div>
 		</div>
