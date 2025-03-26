9ebffe63760ad9e849d479975dc533fac2bf840c
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-26 15:08:28 +0000

    feat(12): add mobile sidebar

---
+++ services/spotter/src/components/TheIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SvgIcon from "@jamescoyle/vue-icon";
+
+interface Icon {
+	icon: string;
+	size?: string;
+}
+
+defineProps<Icon>();
+</script>
+
+<template>
+	<SvgIcon
+		type="mdi"
+		:path="icon"
+		:size="size"
+	/>
+</template>
