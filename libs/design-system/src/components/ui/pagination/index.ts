866e05fba0a0272bf75b55a250ef04082d976f5f
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-27 14:32:35 +0000

    feat(13): add result pagination

---
+++ libs/design-system/src/components/ui/pagination/index.ts
@@ -0,0 +1,10 @@
+export { default as DSPaginationEllipsis } from "./DSPaginationEllipsis.vue";
+export { default as DSPaginationFirst } from "./DSPaginationFirst.vue";
+export { default as DSPaginationLast } from "./DSPaginationLast.vue";
+export { default as DSPaginationNext } from "./DSPaginationNext.vue";
+export { default as DSPaginationPrev } from "./DSPaginationPrev.vue";
+export {
+	PaginationRoot as DSPagination,
+	PaginationList as DSPaginationList,
+	PaginationListItem as DSPaginationListItem,
+} from "reka-ui";
--- services/spotter/src/domains/search/pages/SearchResultsPage.vue
+++ services/spotter/src/domains/search/pages/SearchResultsPage.vue
@@ -2,32 +2,53 @@
 import { mdiViewGrid, mdiFormatListBulletedSquare } from "@mdi/js";
 import DocumentResultCard from "../components/DocumentResultCard.vue";
 import DocumentResultRow from "../components/DocumentResultRow.vue";
+import SearchResutPagination from "../components/SearchResutPagination.vue";
+import { computed, ref } from "vue";
 
 // Mock data
 const START_INDEX = 1;
-const documents = Array.from({ length: 10 }, (unused, index) => ({
+const documents = Array.from({ length: 18 }, (unused, index) => ({
 	id: index + START_INDEX,
 	title: `Document ${index + START_INDEX}`,
 	description: `
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-		sed do eiusmod tempor incididunt ut labore etdolore magna aliqua.
-		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	`,
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore etdolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    `,
 	author: "Albert Einstein",
 	imageUrl: "https://picsum.photos/100/100",
 }));
+const START_PAGE = 1;
+const currentPage = ref(START_PAGE);
+const productPerPage = 12;
+
+const PAGE_OFFSET = 1;
+const paginatedDocuments = computed(() => {
+	const start = (currentPage.value - PAGE_OFFSET) * productPerPage;
+	const end = start + productPerPage;
+	return documents.slice(start, end);
+});
+// End mock data
+
+function handlePageChange(page: number) {
+	currentPage.value = page;
+	window.scrollTo({ top: 0 });
+}
 </script>
 
 <template>
-	<section class="min-h-[calc(100vh-6rem-2rem)] space-y-12">
+	<section class="min-h-[calc(100vh-6rem-2rem)] space-y-12 flex flex-col">
 		<DSInputSearch
 			button-text="Rechercher"
 			class="sticky top-28 z-10 w-full max-w-xl mx-auto"
 		/>
 
-		<DSTabs default-value="cards">
+		<DSTabs
+			v-if="documents && documents.length > 0"
+			default-value="cards"
+		>
 			<DSTabsList class="w-36 grid grid-cols-2">
 				<DSTabsTrigger
 					value="cards"
@@ -44,10 +65,18 @@ const documents = Array.from({ length: 10 }, (unused, index) => ({
 				</DSTabsTrigger>
 			</DSTabsList>
 
+			<SearchResutPagination
+				:total="documents.length"
+				:current-page="currentPage"
+				:product-per-page="productPerPage"
+				@update="handlePageChange"
+				:key="'top-pagination-' + currentPage"
+			/>
+
 			<DSTabsContent value="cards">
 				<div class="w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
 					<DocumentResultCard
-						v-for="document in documents"
+						v-for="document in paginatedDocuments"
 						:key="document.id"
 						:document="document"
 					/>
@@ -57,12 +86,29 @@ const documents = Array.from({ length: 10 }, (unused, index) => ({
 			<DSTabsContent value="rows">
 				<div class="w-full max-w-5xl mx-auto">
 					<DocumentResultRow
-						v-for="document in documents"
+						v-for="document in paginatedDocuments"
 						:key="document.id"
 						:document="document"
 					/>
 				</div>
 			</DSTabsContent>
+
+			<SearchResutPagination
+				:total="documents.length"
+				:current-page="currentPage"
+				:product-per-page="productPerPage"
+				@update="handlePageChange"
+				:key="'bottom-pagination-' + currentPage"
+			/>
 		</DSTabs>
+
+		<div
+			v-else
+			class="grow flex items-center justify-center"
+		>
+			<p class="text-2xl text-gray-500">
+				No documents found
+			</p>
+		</div>
 	</section>
 </template>
