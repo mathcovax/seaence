a757fbb2f3a31d682290fd3a70b782a9c91b868d
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-27 12:33:46 +0000

    feat(13): add search result page

---
+++ services/spotter/src/domains/search/components/DocumentResultRow.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Document {
+	id: number;
+	title: string;
+	description: string;
+	author: string;
+	imageUrl: string;
+}
+
+defineProps<{
+	document: Document;
+}>();
+</script>
+
+<template>
+	<div class="flex items-start gap-4 p-4 border-b border-muted">
+		<!-- Image -->
+		<img
+			:src="document.imageUrl"
+			alt="document"
+			class="w-24 h-24 object-cover rounded-md"
+		>
+
+		<!-- Content -->
+		<div class="flex-1">
+			<h3 class="text-lg font-semibold text-primary">
+				{{ document.title }}
+			</h3>
+
+			<p class="text-sm text-muted-foreground line-clamp-3">
+				{{ document.description }}
+			</p>
+
+			<p class="mt-2 text-xs text-muted-foreground">
+				Auteur : {{ document.author }}
+			</p>
+		</div>
+	</div>
+</template>
