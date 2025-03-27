6c63e8cbfd61fd4497cef888f50b6ae3c616bfc9
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-03-27 12:33:46 +0000

    feat(13): add search result page

--- libs/design-system/src/components/ui/tabs/DSTabsTrigger.vue
+++ libs/design-system/src/components/ui/tabs/DSTabsTrigger.vue
@@ -18,7 +18,7 @@ const forwardedProps = useForwardProps(delegatedProps);
 	<TabsTrigger
 		v-bind="forwardedProps"
 		:class="cn(
-			'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-6 py-3 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-blue-seaence data-[state=active]:text-white data-[state=active]:shadow-sm',
+			'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-6 py-3 text-sm font-medium ring-offset-background cursor-pointer transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-blue-seaence data-[state=active]:text-white data-[state=active]:shadow-sm',
 			props.class,
 		)"
 	>
