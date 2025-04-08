c23c876ff543ed7ae89f3d98268626376dba5ac7
mathcovax <campani.mathieu@gmail.com>
2025-04-08 08:19:49 +0000

    feat(19): refacto worker

--- services/marine-snow/interfaces/repositories/mission.ts
+++ services/marine-snow/interfaces/repositories/mission.ts
@@ -1,6 +1,7 @@
 import { missionRepository, type Mission } from "@business/applications/repositories/mission";
 import { missionIdObjecter } from "@business/domains/entities/mission";
 import { SearchResultPubMedMissionEntity } from "@business/domains/entities/mission/searchResult/pubMed";
+import { SendSearchResultMissionEntity } from "@business/domains/entities/mission/sendSearchResult";
 import { prismaClient } from "@interfaces/providers/prisma";
 import { match, P } from "ts-pattern";
 import { uuidv7 } from "uuidv7";
@@ -28,6 +29,33 @@ missionRepository.default = {
 							searchDateTo: interval.to,
 						},
 					});
+				},
+			)
+			.with(
+				{ entity: P.instanceOf(SendSearchResultMissionEntity) },
+				async({ entity }) => {
+					const { resultDetails, ...restSimpleEntity } = entity.toSimpleObject();
+
+					await prismaClient.sendSearchResultMission.upsert({
+						where: {
+							id: restSimpleEntity.id,
+						},
+						create: restSimpleEntity,
+						update: restSimpleEntity,
+					});
+
+					if (resultDetails) {
+						await prismaClient.resultDetailsSendSearchResultMission.upsert({
+							where: {
+								missionId: restSimpleEntity.id,
+							},
+							create: {
+								missionId: restSimpleEntity.id,
+								...resultDetails,
+							},
+							update: resultDetails,
+						});
+					}
 				},
 			)
 			.exhaustive();
--- services/marine-snow/interfaces/usecase/index.ts
+++ services/marine-snow/interfaces/usecase/index.ts
@@ -2,7 +2,7 @@ import "../repositories";
 import { CreateSearchResultPubMedMissionUsecase } from "@business/applications/usecases/missions/searchResult/pubMed/createSearchResultPubMedMission";
 import { StartSearchResultMissionUsecase } from "@business/applications/usecases/missions/searchResult/startSearchResultMission";
 import { CreateSendSearchResultMissionUsecase } from "@business/applications/usecases/missions/sendSearchResult/createSendSearchResultMission";
-import { StartSendSearchResultMissionUsecase } from "@business/applications/usecases/missions/sendSearchResult/sendSearchResultMission";
+import { StartSendSearchResultMissionUsecase } from "@business/applications/usecases/missions/sendSearchResult/startSendSearchResultMission";
 
 export const startSearchResultMissionUsecase = new StartSearchResultMissionUsecase();
 export const createSearchResultPubMedMissionUsecase = new CreateSearchResultPubMedMissionUsecase();
