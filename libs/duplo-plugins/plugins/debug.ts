eb699b10ba3c60cb0a18d46e8186be58b5f71c74
mathcovax <campani.mathieu@gmail.com>
2025-04-25 09:30:57 +0000

    fix(hf): deploy

--- libs/duplo-plugins/plugins/debug.ts
+++ libs/duplo-plugins/plugins/debug.ts
@@ -2,13 +2,11 @@ import { Duplo } from "@duplojs/core";
 
 export function debug(){
 	return function(instance: Duplo) {
-		if(instance.config.environment === "DEV") {
-			instance.hook(
-				"onError",
-				(request, error) => {
-					console.log(`${request.method}:${request.path}`, error);
-				}
-			)
-		}
+		instance.hook(
+			"onError",
+			(request, error) => {
+				console.log(`${request.method}:${request.path}`, error);
+			}
+		)
 	}
 }
