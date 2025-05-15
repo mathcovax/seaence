126dce51df8abbf6d5ffdf75baa2029ff564262f
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-05-15 13:09:36 +0000

    feat(53): add mail template

--- services/bottle/interfaces/repositories/notification.ts
+++ services/bottle/interfaces/repositories/notification.ts
@@ -7,6 +7,7 @@ import { EmailProvider } from "@interfaces/providers/email";
 import { notificationRepository } from "@business/applications/repositories/notification";
 import { notificationIdObjecter } from "@business/domains/entities/notification/base";
 import { RegisterNotificationEntity } from "@business/domains/entities/notification/register";
+import { welcomeTemplate } from "@interfaces/providers/email/templates/signIn";
 
 notificationRepository.default = {
 	generateNotificationId() {
@@ -53,9 +54,8 @@ notificationRepository.default = {
 				async() => {
 					await EmailProvider.send({
 						to: user.email,
-						subject: "Register",
-						// TODO: add template
-						content: "",
+						subject: "Bienvenue sur Seaence !",
+						content: welcomeTemplate(user.username, "https://seaence.org/"),
 						from: envs.NO_REPLY_EMAIL,
 					});
 				},
