dcf816a1e9bc3b5804a5df243baca7e4acfd41f9
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-06-18 11:03:02 +0000

    feat(84): fix module card

--- services/spotter/src/i18n/languages/FR-fr.ts
+++ services/spotter/src/i18n/languages/FR-fr.ts
@@ -252,7 +252,7 @@ export const FRfr = {
 			},
 			footer: {
 				nav: {
-					conditions: "Conditions d'utilisation",
+					conditions: "Conditions générales d'utilisation",
 					about: "À propos",
 					support: "Support",
 				},
