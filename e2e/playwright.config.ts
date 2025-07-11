34cbc9103d4759a675220f3ddf7b595ac2211781
mathcovax <campani.mathieu@gmail.com>
2025-07-11 17:31:38 +0000

    feat(95): add CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -7,11 +7,12 @@ export default defineConfig({
 	forbidOnly: envs.CI,
 	retries: envs.RETRIES,
 	workers: envs.WORKER,
-	reporter: "html",
+	reporter: [["html", { open: "never" }]],
 	use: {
 		headless: true,
 		baseURL: envs.BASE_URL,
 		trace: "on",
+		screenshot: "only-on-failure",
 		launchOptions: {
 			executablePath: "/usr/bin/chromium-browser",
 			args: ["--no-sandbox", "--disable-dev-shm-usage"],
