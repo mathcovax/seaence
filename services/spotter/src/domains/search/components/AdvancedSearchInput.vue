efd054d6c8f7ed48a2808df936863bed50670c20
Maubry94 <matthieu.aubry94370@gmail.com>
2025-07-18 12:45:01 +0200

    feat(109): add tutorial link

--- services/spotter/src/domains/search/components/AdvancedSearchInput.vue
+++ services/spotter/src/domains/search/components/AdvancedSearchInput.vue
@@ -42,7 +42,7 @@ function onSubmit() {
 		class="flex flex-col gap-4 pt-1"
 		@submit.prevent="onSubmit"
 	>
-		<div class="flex gap-4">
+		<div class="flex gap-4 items-center">
 			<DSPrimaryButton type="submit">
 				{{ $t("cta.search") }}
 			</DSPrimaryButton>
@@ -59,6 +59,13 @@ function onSubmit() {
 			<AddToFavoriteEquation
 				v-model="modelValue"
 			/>
+
+			<RouterLink
+				:to="scratchTutorialPage"
+				class="text-blue-seaence hover:underline"
+			>
+				{{ $t("cta.howItWorks") }}
+			</RouterLink>
 		</div>
 
 		<TheScratch
