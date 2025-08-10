7e197636b51899868a8b1224c0f5dd8131a2b737
mathcovax <campani.mathieu@gmail.com>
2025-08-10 17:04:41 +0000

    fix(hf): trad

--- services/spotter/src/i18n/languages/FR-fr.ts
+++ services/spotter/src/i18n/languages/FR-fr.ts
@@ -427,7 +427,7 @@ export const FRfr = {
 			high: "Boost élevé",
 		},
 		foundResults: "{count} résultat(s) trouvé(s)",
-		foundMoreThan2000Results: "Plus de 2000 ésultat trouvés",
+		foundMoreThan2000Results: "Plus de 2000 résultat trouvés",
 		noResult: "Aucun résultat trouvé",
 	},
 	documentRowResult: {
