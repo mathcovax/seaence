b96b1fabbf528d7fd07f9dcbb2b11c04f6c189a2
Matthieu Aubry <58041322+Maubry94@users.noreply.github.com>
2025-08-26 13:05:51 +0000

    feat(122): fix useless router history on document language change

--- services/spotter/src/components/BackButton.vue
+++ services/spotter/src/components/BackButton.vue
@@ -1,12 +1,37 @@
 <script setup lang="ts">
+import type { RouteLocationRaw } from "vue-router";
+
+interface Props {
+	to?: RouteLocationRaw | { createTo(...args: unknown[]): RouteLocationRaw };
+	replaceInHistory?: boolean;
+}
+
+const props = defineProps<Props>();
+
 const router = useRouter();
+
+function handleClick() {
+	if (props.to) {
+		const route = typeof props.to === "object" && "createTo" in props.to
+			? props.to.createTo()
+			: props.to;
+
+		if (props.replaceInHistory) {
+			void router.replace(route);
+		} else {
+			void router.push(route);
+		}
+	} else {
+		router.back();
+	}
+}
 </script>
 
 <template>
 	<DSOutlineButton
 		square
 		class="shrink-0"
-		@click="router.back()"
+		@click="handleClick"
 	>
 		<DSIcon name="arrowLeft" />
 	</DSOutlineButton>
